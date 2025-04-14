05f3152aaa32d6cb7ff085dddcbccb7197eb9700
draw-n <helen.wu1219@gmail.com>
2025-04-14 17:25:43 -0500

    we want original name

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(
             null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+            file.originalname
         );
     },
     fileFilter: function (req, file, callback) {
