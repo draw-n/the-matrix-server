6eb93c12c338fa97e00c828e7df8614ea5f98682
draw-n <helen.wu1219@gmail.com>
2025-09-02 22:13:43 -0500

    async problem?

--- services/slicer.service.js
+++ services/slicer.service.js
@@ -3,26 +3,28 @@ const { moveFile } = require("../utils/file.utils.js");
 const sliceMeshToGcode = (fileName, options) => {
     const filePath = `${process.env.MESH_INPUT_DIR || "meshes"}/${fileName}`;
     console.log("Slicing file:", fileName, "with options:", options);
-    exec(
-        `./slicer-cli/superslicer/bin/superslicer -g "${filePath}"`,
-        (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
+    return new Promise((resolve, reject) => {
+        exec(
+            `./slicer-cli/superslicer/bin/superslicer -g "${filePath}"`,
+            (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject(error);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+
+                const gcodeFileName = fileName.replace(/\.[^/.]+$/, ".gcode");
+                // move gcode file to gcode folder
+                moveFile(
+                    filePath.replace(/\.[^/.]+$/, ".gcode"),
+                    `${process.env.GCODE_OUTPUT_DIR || "gcodes"}/${gcodeFileName}`
+                );
+                resolve(gcodeFileName); // should be returning file name
             }
-            console.log(`stdout: ${stdout}`);
-            console.error(`stderr: ${stderr}`);
-        }
-    );
-
-    gcodeFileName = fileName.replace(/\.[^/.]+$/, ".gcode");
-    // move gcode file to gcode folder
-    moveFile(
-        filePath.replace(/\.[^/.]+$/, ".gcode"),
-        `${process.env.GCODE_OUTPUT_DIR || "gcodes"}/${gcodeFileName}`
-    );
-
-    return gcodeFileName; // should be returning file name
+        );
+    });
 };
 
 module.exports = { sliceMeshToGcode };
