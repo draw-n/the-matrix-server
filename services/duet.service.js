12b04e43700efea15d407cfb94a0793a58de0149
draw-n <helen.wu1219@gmail.com>
2025-09-03 11:31:41 -0500

    removing newline with colon

--- services/duet.service.js
+++ services/duet.service.js
@@ -40,7 +40,7 @@ const sendGcodeToDuet = async (printerIp, fileName, filePath) => {
  */
 const startPrint = async (printerIp) => {
     const response = await axios.get(`http://${printerIp}/rr_gcode`, {
-        params: { gcode: `M23\nM24` },
+        params: { gcode: `M23;M24` },
     });
     return response.data;
 };
