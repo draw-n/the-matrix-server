4f39ff354d2bda7beacfa88238bf9009a59c7b1c
draw-n <helen.wu1219@gmail.com>
2025-09-02 22:34:28 -0500

    trying

--- services/slicer.service.js
+++ services/slicer.service.js
@@ -1,14 +1,17 @@
 const { exec } = require("child_process");
 const { moveFile } = require("../utils/file.utils.js");
+const path = require("path");
 const sliceMeshToGcode = (fileName, options) => {
     console.log("Slicing file:", fileName, "with options:", options);
     return new Promise((resolve, reject) => {
-        const filePath = `${
-            process.env.MESH_INPUT_DIR || "meshes"
-        }/${fileName}`;
+        const filePath =
+            (process.env.MESH_INPUT_DIR || "meshes") + "/" + fileName;
         const gcodeFileName = fileName.replace(/\.[^/.]+$/, ".gcode");
+        const outputPath = path.resolve(meshDir, gcodeFileName);
+        const finalGcodePath = path.resolve(gcodeDir, gcodeFileName);
+        X;
         exec(
-            `./slicer-cli/superslicer/bin/superslicer --output ${gcodeFileName} -g "${filePath}"`,
+            `./slicer-cli/superslicer/bin/superslicer --output ${finalGcodePath} -g "${filePath}"`,
             (error, stdout, stderr) => {
                 if (error) {
                     console.error(`exec error: ${error}`);
@@ -20,12 +23,7 @@ const sliceMeshToGcode = (fileName, options) => {
 
                 console.log("gcode:" + gcodeFileName);
                 // move gcode file to gcode folder
-                moveFile(
-                    filePath.replace(/\.[^/.]+$/, ".gcode"),
-                    `${
-                        process.env.GCODE_OUTPUT_DIR || "gcodes"
-                    }/${gcodeFileName}`
-                );
+                moveFile(outputPath, finalGcodePath);
                 resolve(gcodeFileName); // should be returning file name
             }
         );
