b466c158402d24b204dac681b95c0af3e4ca32b3
draw-n <helen.wu1219@gmail.com>
2025-09-25 08:55:52 -0500

    updating file size

--- routes/jobs.router.js
+++ routes/jobs.router.js
@@ -8,9 +8,10 @@ const storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, process.env.MESH_INPUT_DIR || "uploads/");
     },
+    limits: { fileSize: 250 * 1024 * 1024 }, // 250 MB limit
     filename: function (req, file, callback) {
         callback(null, file.originalname);
-    }
+    },
 });
 
 const upload = multer({ storage: storage });
